490a619a82df41bf6a382ec9fe614acbf6f9a31c
John Cornyn <JMIHC@users.noreply.github.com>
2021-06-10 21:01:12 -0700

    mending ways

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,5 @@
 import '../styles/globals.css'
 
-import Amplify from 'aws-amplify';
-import config from '../aws-exports';
-Amplify.configure({
-  ...config, ssr: true
-});
-
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
